95cec06534aeeca1350395fb4e13a91aa34856ba
Robert Lewoń <lewon.robert@gmail.com>
2017-05-21 13:49:37 +0200

    fix

--- tests/TestTest.php
+++ tests/TestTest.php
@@ -1,8 +1,8 @@
 <?php
 
 // backward compatibility
-if (class_exists('\\PHPUnit\\Framework\\TestCase') && !class_exists('\\PHPUnit_Framework_TestCase')) {
-    class_alias('\\PHPUnit\\Framework\\TestCase', '\\PHPUnit_Framework_TestCase');
+if (class_exists('\PHPUnit\Framework\TestCase') && !class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
 
 class TestTest extends \PHPUnit_Framework_TestCase
