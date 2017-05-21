21c8f7d3f5fdbe8d7537512733f4c6f71f011f5b
Robert Lewoń <lewon.robert@gmail.com>
2017-05-21 12:25:22 +0200

    init

---
+++ tests/TestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+*/
+
+class TestTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAutorizationSuccess()
+    {
+        $this->assertTrue(true);
+    }
+}
